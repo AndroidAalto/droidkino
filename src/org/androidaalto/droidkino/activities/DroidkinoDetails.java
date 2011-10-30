46276f10c9891b00dc2378c4ddd0affbcfbd8d77
marcos tong <marcos.tong@mobengi.com>
2011-10-30 15:45:47 +0200

    added details fragment activity

--- src/org/androidaalto/droidkino/activities/DroidkinoDetails.java
+++ src/org/androidaalto/droidkino/activities/DroidkinoDetails.java
@@ -1,7 +1,31 @@
 package org.androidaalto.droidkino.activities;
 
-import android.app.Activity;
+import org.androidaalto.droidkino.fragments.MovieDetail;
 
-public class DroidkinoDetails extends Activity {
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class DroidkinoDetails extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE) {
+            // If the screen is now in landscape mode, we can show the
+            // dialog in-line with the list so we don't need this activity.
+            finish();
+            return;
+        }
+
+        if (savedInstanceState == null) {
+            // During initial setup, plug in the details fragment.
+            MovieDetail details = new MovieDetail();
+            details.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+        }
+    }
 
 }
